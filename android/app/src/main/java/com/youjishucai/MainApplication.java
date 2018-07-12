442866b0ae50e145a945d38c779590cc4ee4c679
sssssuper1 <156980742@qq.com>
2018-07-12 15:49:23 +0800

    android图片缓存插件配置

--- android/app/src/main/java/com/youjishucai/MainApplication.java
+++ android/app/src/main/java/com/youjishucai/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.theweflex.react.WeChatPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
           new CookieManagerPackage(),
           new SplashScreenReactPackage(),
-          new WeChatPackage()
+          new WeChatPackage(),
+          new RNFetchBlobPackage()
       );
     }
 
